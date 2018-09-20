d96b22ee5478319d49594351a5dc81226d1c5710
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-20 19:13:00 +0800

    Add map element
    Add map key element

--- script.js
+++ script.js
@@ -34,6 +34,48 @@ const tooltip = d3.select('#chart').append('div')
   .attr('id', 'tooltip')
   .style('opacity', 0)
 
+// Map 
+const path = d3.geoPath()
+
+//Scale
+const xScale = d3.scaleLinear()
+  .domain([1, 10])
+  .rangeRound([(height), (width)])
+
+const color = d3.scaleThreshold()
+  .domain(d3.range(2, 10))
+  .range(d3.schemeBlues[9])
+
+// Map Key
+const mapKey = svg.append('g')
+  .attr('class', 'key')
+  .attr('transform', `translate(0, ${padding})`)
+
+mapKey.selectAll('rect')
+  .data(color.range().map((d) => {
+    d = color.invertExtent(d)
+    if (d[0] == null) {d[0] = xScale.domain()[0]}
+    if (d[1] == null) {d[1] = xScale.domain()[1]}
+    return d
+  }))
+  .enter().append('text')
+  .attr('height', 8)
+  .attr('x', (d) => xScale(d[0]))
+  .attr('width', (d) => xScale(d[1]) - xScale(d[0]))
+  .attr('fill', (d) => color(d[0]))
+
+mapKey.append('text')
+  .attr('class', 'caption')
+  .attr('x', xScale.range()[0])
+  .attr('y', -6)
+  .text('Education Rates')
+
+mapKey.call(d3.axisBottom(xScale)
+      .tickFormat((x, i) => i ? x : `${x}%`)
+      .tickValues(color.domain()))
+    .select('.domain')
+      .remove()
+
 // Get the data
 const eduURL = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json'
 const countiesURL = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'
@@ -44,6 +86,23 @@ const chart = async () => {
   let getUSData = await fetch(countiesURL)
   let rawUSData = await getUSData.json()
   console.log(`rawUSData`, rawUSData)
+  // var USDataId = topojson.feature(rawUSData, rawUSData.objects.counties).features.map(d => d.id)
+  // console.log(`USDataId`, USDataId)
+
+  //Create map
+  svg.append('g')
+    .attr('class', 'county')
+    .selectAll('path')
+    .data(topojson.feature(rawUSData, rawUSData.objects.counties).features)
+    .enter().append('path')
+    .attr('fill', (d) => color(d.id = rawEduData.map(d => d.fips)))
+    .attr('d', path)
+    .append('title')
+      .text((d) => `${d.id}%`)
+  svg.append('path')
+    .datum(topojson.mesh(rawUSData, rawUSData.objects.states, (a, b) => a !== b))
+    .attr('class', 'states')
+    .attr('d', path)
 }
 
 chart()
